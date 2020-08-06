147444e2b72064f8af4763a41ed5894d738389e5
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-08-06 17:11:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,10 +3,13 @@ class Song
   attr_accessor :name, :artist, :genre
 
   @@count = 0
+  @@artists = []
+  
 
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
+    @@artists << artist
     @genre = genre
     @@count += 1
   end
